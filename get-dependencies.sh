ed239c1fa25996da62d4db4412d382fba2b3bcd4
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-04-29 17:51:42 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,9 +8,9 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm gcc-libs cargo
 
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+#echo "Installing debloated packages..."
+#echo "---------------------------------------------------------------"
+#get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
