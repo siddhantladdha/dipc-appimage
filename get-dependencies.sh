4f4730691b7a5bf6f0b497d33ffe1d650cc4d2b9
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-04-29 08:24:15 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm gcc-libs cargo
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -16,7 +16,7 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-
+cargo install dipc
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
 # if [ "${DEVEL_RELEASE-}" = 1 ]; then
